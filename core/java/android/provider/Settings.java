a117f68f4cad4110c03f81de6bd1d44af2ec51f5
SKULSHADY <anushekprasal@gmail.com>
2022-07-23 21:05:01 +0530

    base: Double tap to trigger doze [1/2] Thanks to yank555-lu for the original commit YAOSP-NG/frameworks_base@d6ac164. Cleaned up and ported to Android 10 by SKULSHADY.
    Change-Id: I4154925a0c7de37f1dc84b58b280691cc44d14c9

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5379,6 +5379,12 @@ public final class Settings {
          * @hide
          */
         public static final String VOLUME_ROCKER_WAKE = "volume_rocker_wake";
+        
+        /**
+         * Use doubletap as doze pulse triggers
+         * @hide
+         */
+        public static final String DOZE_TRIGGER_DOUBLETAP = "doze_trigger_doubletap";
 
         /**
          * @hide
